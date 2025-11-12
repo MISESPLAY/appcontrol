2a013c6402bc80809f7b9eadd842acf0e7720d61
Jonathan <cantujonathan16.music@gmail.com>
2025-11-11 21:09:05 -0600

    corrigiendo archivo de controller

--- app/Models/ORGANIGRAMA/PERSISTENCE/ELOQUENT/MODELS/OrganigramaModel.php
+++ app/Models/ORGANIGRAMA/PERSISTENCE/ELOQUENT/MODELS/OrganigramaModel.php
@@ -2,10 +2,12 @@
 
 namespace app\Models\ORGANIGRAMA\PERSISTENCE\ELOQUENT\MODELS;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class OrganigramaModel extends Model
 {
-    //
-    protected $table = "organizacion"; 
-}
+    use HasFactory; 
+    protected 
+} 
